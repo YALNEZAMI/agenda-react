018b61e7c95db03367d55d57a6f82aedc11c242e
YALNEZAMI <yaseralnezami@gmail.com>
2024-10-06 17:32:58 +0200

    refact: using new components in the main one(app)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { Add } from "./components/add/add";
+import { Header } from "./components/header/header";
+import { Task } from "./components/task/task";
 function App() {
+  const storage = JSON.parse(localStorage.getItem("tasks"));
+  const [tasks, setTasks] = useState([]);
+  const setInStorage = (ts) => {
+    localStorage.setItem("tasks", JSON.stringify(ts));
+  };
+  if (storage != null) {
+    setTimeout(() => {
+      setTasks(storage);
+    }, 10);
+  } else {
+    setInStorage([]);
+  }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter((t) => t.id !== id);
+    setTasks(filteredTasks);
+    setInStorage(filteredTasks);
+  };
+  const updatingTask = (newTask) => {
+    const newTasks = tasks.map((t) => {
+      if (t.id === newTask.id) {
+        return newTask;
+      } else {
+        return t;
+      }
+    });
+    setTasks(newTasks);
+    setInStorage(newTasks);
+  };
+  const allTasks = tasks.map((t) => (
+    <Task
+      updatedEvent={updatingTask}
+      deleteEvent={deleteTask}
+      key={t.id}
+      task={t}
+    ></Task>
+  ));
+  const taskAdded = (task) => {
+    const newTasks = tasks.map((t) => t);
+    newTasks.push(task);
+    setTasks(newTasks);
+    console.log(newTasks);
+
+    setInStorage(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <Header></Header>
+      <Add taskAddedEvent={taskAdded}></Add>
+      <hr></hr>
+      {allTasks.length === 0 && (
+        <p className="text-center text-xl mt-16 underline">
+          You have no tasks yet, add some !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      <div className="flex flex-wrap mt-4">{allTasks}</div>
     </div>
   );
 }
