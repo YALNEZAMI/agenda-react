09c7e73f2d2fc4500680542b2a810358aaf0f70d
YALNEZAMI <yaseralnezami@gmail.com>
2024-10-06 17:31:45 +0200

    init : header of the app

---
+++ src/components/header/header.js
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <h1 className="bg-blue-300 shadow-sm rounded p-4 text-center font-bold text-3xl">
+      <b className="underline">Remenda</b>, Your favorite tasks bloc !
+    </h1>
+  );
+};
